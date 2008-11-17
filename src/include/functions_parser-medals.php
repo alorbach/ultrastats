985ffa15c136dfe43060437fd3f0ad8321db837b
unknown <Delta@.(none)>
2008-11-17 23:53:12 +0100

    Fixed KNife medal for CodWW and fixed minor bug in the medals page template

--- src/include/functions_parser-medals.php
+++ src/include/functions_parser-medals.php
@@ -131,24 +131,6 @@ function CreateMedalsSQLCode( $serverid, $includeTimeFilter = false )
 			$content['gen_gameversion'] == COD2 || 
 			$content['gen_gameversion'] == CODWW )
 	{
-		$content['medals']['medal_pro_slappyhappy']['DisplayName'] = "Slappy Happy";
-		$content['medals']['medal_pro_slappyhappy']['GroupedPlayerID'] = "PLAYERID";
-		$content['medals']['medal_pro_slappyhappy']['sql'] = "SELECT " .
-					STATS_PLAYER_KILLS . ".PLAYERID, " .
-					" sum(" . STATS_PLAYER_KILLS . ".Kills) as AllKills" . 
-					" FROM " . STATS_PLAYER_KILLS . 
-					" INNER JOIN (" . STATS_DAMAGETYPES . ", " . STATS_ROUNDS . 
-					") ON (" . 
-					STATS_DAMAGETYPES . ".ID=" . STATS_PLAYER_KILLS . ".DAMAGETYPEID AND " . 
-					STATS_PLAYER_KILLS . ".ROUNDID=" . STATS_ROUNDS . ".ID " . 
-					") " . 
-					" WHERE " . STATS_DAMAGETYPES . ".DAMAGETYPE IN ('MOD_MELEE') " . 
-					GetCustomServerWhereQuery(STATS_PLAYER_KILLS, false, false, $serverid) . 
-					GetBannedPlayerWhereQuery(STATS_PLAYER_KILLS, "PLAYERID", false) . 
-					$szTimeFilter . 
-					" GROUP BY " . STATS_PLAYER_KILLS . ".PLAYERID " . 
-					" ORDER BY AllKills";
-
 		$content['medals']['medal_pro_sniper']['DisplayName'] = "Sniper";
 		$content['medals']['medal_pro_sniper']['GroupedPlayerID'] = "PLAYERID";
 		$content['medals']['medal_pro_sniper']['sql'] = "SELECT " .
@@ -167,26 +149,8 @@ function CreateMedalsSQLCode( $serverid, $includeTimeFilter = false )
 					" GROUP BY " . STATS_PLAYER_KILLS . ".PLAYERID " . 
 					" ORDER BY AllKills";
 	}
-	else if($content['gen_gameversion'] == COD4)
+	else if($content['gen_gameversion'] == COD4 )
 	{
-		$content['medals']['medal_pro_knifekills']['DisplayName'] = "Knife Kills";
-		$content['medals']['medal_pro_knifekills']['GroupedPlayerID'] = "PLAYERID";
-		$content['medals']['medal_pro_knifekills']['sql'] = "SELECT " . 
-					STATS_PLAYER_KILLS . ".PLAYERID, " . 
-					" sum(" . STATS_PLAYER_KILLS . ".Kills) as AllKills" . 
-					" FROM " . STATS_PLAYER_KILLS . 
-					" INNER JOIN (" . STATS_DAMAGETYPES . ", " . STATS_ROUNDS . 
-					") ON (" . 
-					STATS_DAMAGETYPES . ".ID=" . STATS_PLAYER_KILLS . ".DAMAGETYPEID AND " . 
-					STATS_PLAYER_KILLS . ".ROUNDID=" . STATS_ROUNDS . ".ID " . 
-					") " . 
-					" WHERE " . STATS_DAMAGETYPES . ".DAMAGETYPE IN ('MOD_MELEE') " . 
-					GetCustomServerWhereQuery(STATS_PLAYER_KILLS, false, false, $serverid) . 
-					GetBannedPlayerWhereQuery(STATS_PLAYER_KILLS, "PLAYERID", false) . 
-					$szTimeFilter . 
-					" GROUP BY " . STATS_PLAYER_KILLS . ".PLAYERID " . 
-					" ORDER BY AllKills";
-
 		$content['medals']['medal_pro_sniper']['DisplayName'] = "Sniper";
 		$content['medals']['medal_pro_sniper']['GroupedPlayerID'] = "PLAYERID";
 		$content['medals']['medal_pro_sniper']['sql'] = "SELECT " . 
@@ -217,6 +181,51 @@ function CreateMedalsSQLCode( $serverid, $includeTimeFilter = false )
 					STATS_PLAYER_KILLS . ".ROUNDID=" . STATS_ROUNDS . ".ID " . 
 					") " . 
 					" WHERE " . STATS_WEAPONS . ".INGAMENAME IN ('c4_mp', 'claymore_mp') " . 
+					GetCustomServerWhereQuery(STATS_PLAYER_KILLS, false, false, $serverid) . 
+					GetBannedPlayerWhereQuery(STATS_PLAYER_KILLS, "PLAYERID", false) . 
+					$szTimeFilter . 
+					" GROUP BY " . STATS_PLAYER_KILLS . ".PLAYERID " . 
+					" ORDER BY AllKills";
+	}
+	
+	// Slappy Happy Medal ;)
+	if (	$content['gen_gameversion'] == COD || 
+			$content['gen_gameversion'] == CODUO || 
+			$content['gen_gameversion'] == COD2 ) 
+	{
+		$content['medals']['medal_pro_slappyhappy']['DisplayName'] = "Slappy Happy";
+		$content['medals']['medal_pro_slappyhappy']['GroupedPlayerID'] = "PLAYERID";
+		$content['medals']['medal_pro_slappyhappy']['sql'] = "SELECT " .
+					STATS_PLAYER_KILLS . ".PLAYERID, " .
+					" sum(" . STATS_PLAYER_KILLS . ".Kills) as AllKills" . 
+					" FROM " . STATS_PLAYER_KILLS . 
+					" INNER JOIN (" . STATS_DAMAGETYPES . ", " . STATS_ROUNDS . 
+					") ON (" . 
+					STATS_DAMAGETYPES . ".ID=" . STATS_PLAYER_KILLS . ".DAMAGETYPEID AND " . 
+					STATS_PLAYER_KILLS . ".ROUNDID=" . STATS_ROUNDS . ".ID " . 
+					") " . 
+					" WHERE " . STATS_DAMAGETYPES . ".DAMAGETYPE IN ('MOD_MELEE') " . 
+					GetCustomServerWhereQuery(STATS_PLAYER_KILLS, false, false, $serverid) . 
+					GetBannedPlayerWhereQuery(STATS_PLAYER_KILLS, "PLAYERID", false) . 
+					$szTimeFilter . 
+					" GROUP BY " . STATS_PLAYER_KILLS . ".PLAYERID " . 
+					" ORDER BY AllKills";
+	}
+	else if($content['gen_gameversion'] == COD4 ||
+			$content['gen_gameversion'] == CODWW )
+	{
+		$content['medals']['medal_pro_knifekills']['DisplayName'] = "Knife Kills";
+		$content['medals']['medal_pro_knifekills']['GroupedPlayerID'] = "PLAYERID";
+		$content['medals']['medal_pro_knifekills']['sql'] = "SELECT " . 
+					STATS_PLAYER_KILLS . ".PLAYERID, " . 
+					" sum(" . STATS_PLAYER_KILLS . ".Kills) as AllKills" . 
+					" FROM " . STATS_PLAYER_KILLS . 
+					" INNER JOIN (" . STATS_DAMAGETYPES . ", " . STATS_ROUNDS . 
+					") ON (" . 
+					STATS_DAMAGETYPES . ".ID=" . STATS_PLAYER_KILLS . ".DAMAGETYPEID AND " . 
+					STATS_PLAYER_KILLS . ".ROUNDID=" . STATS_ROUNDS . ".ID " . 
+					") " . 
+					" WHERE " . STATS_DAMAGETYPES . ".DAMAGETYPE IN ('MOD_MELEE') " . 
 					GetCustomServerWhereQuery(STATS_PLAYER_KILLS, false, false, $serverid) . 
 					GetBannedPlayerWhereQuery(STATS_PLAYER_KILLS, "PLAYERID", false) . 
 					$szTimeFilter . 
@@ -407,8 +416,7 @@ function CreateAllMedals( $serverid )
 
 	if (	$content['gen_gameversion'] == COD || 
 			$content['gen_gameversion'] == CODUO || 
-			$content['gen_gameversion'] == COD2 ||
-			$content['gen_gameversion'] == CODWW )
+			$content['gen_gameversion'] == COD2 )
 	{
 		// --- Calc: medal_pro_slappyhappy
 		if ( $content["medal_pro_slappyhappy"] == "yes" ) 
@@ -430,7 +438,37 @@ function CreateAllMedals( $serverid )
 				PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_slappyhappy is empty!" );
 		}
 		// --- 
+	}
+	else if($content['gen_gameversion'] == COD4 ||
+			$content['gen_gameversion'] == CODWW )
+	{
+		// --- Calc: medal_pro_knifekills 
+		if ( $content["medal_pro_knifekills"] == "yes" ) 
+		{
+			$sqlquery =	$content['medals']['medal_pro_knifekills']['sql'] . " DESC LIMIT 1";
+			PrintHTMLDebugInfo( DEBUG_ULTRADEBUG, "Medal", "medal_pro_knifekills: " . $sqlquery ); 
 
+			$topplayer = ReturnMedalValue($sqlquery); 
+			if ( isset($topplayer['PLAYERID']) && $topplayer['AllKills'] > 0 ) 
+				InsertOrUpdateMedalValue(  "medal_pro_knifekills", 
+										   $content['medals']['medal_pro_knifekills']['DisplayName'], 
+										   $serverid, 
+										   "medal_pro_knifekills", 
+										   $topplayer['AllKills'], 
+										   "Kills", 
+										   $topplayer['PLAYERID'], 
+										   3 ); 
+			else 
+				PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_knifekills is empty!" ); 
+		}
+		// ---
+	}
+
+	if (	$content['gen_gameversion'] == COD || 
+			$content['gen_gameversion'] == CODUO || 
+			$content['gen_gameversion'] == COD2 ||
+			$content['gen_gameversion'] == CODWW )
+	{
 		// --- Calc: medal_pro_sniper
 		if ( $content["medal_pro_sniper"] == "yes" ) 
 		{
@@ -455,27 +493,6 @@ function CreateAllMedals( $serverid )
 	}
 	else if($content['gen_gameversion'] == COD4)
 	{
-		// --- Calc: medal_pro_knifekills 
-		if ( $content["medal_pro_knifekills"] == "yes" ) 
-		{
-			$sqlquery =	$content['medals']['medal_pro_knifekills']['sql'] . " DESC LIMIT 1";
-			PrintHTMLDebugInfo( DEBUG_ULTRADEBUG, "Medal", "medal_pro_knifekills: " . $sqlquery ); 
-
-			$topplayer = ReturnMedalValue($sqlquery); 
-			if ( isset($topplayer['PLAYERID']) && $topplayer['AllKills'] > 0 ) 
-				InsertOrUpdateMedalValue(  "medal_pro_knifekills", 
-										   $content['medals']['medal_pro_knifekills']['DisplayName'], 
-										   $serverid, 
-										   "medal_pro_knifekills", 
-										   $topplayer['AllKills'], 
-										   "Kills", 
-										   $topplayer['PLAYERID'], 
-										   3 ); 
-			else 
-				PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_knifekills is empty!" ); 
-		}
-		// ---
-
 		// --- Calc: medal_pro_sniper 
 		if ( $content["medal_pro_sniper"] == "yes" ) 
 		{
@@ -496,6 +513,28 @@ function CreateAllMedals( $serverid )
 				PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_sniper is empty!" ); 
 		}
 		// ---
+
+		// --- Calc: medal_pro_claymorec4
+		if ( $content["medal_pro_claymorec4"] == "yes" ) 
+		{
+			// Credits for this medal go to: [-UFC-]James Ryan (matze1)
+			$sqlquery =	$content['medals']['medal_pro_claymorec4']['sql'] . " DESC LIMIT 1";
+			PrintHTMLDebugInfo( DEBUG_ULTRADEBUG, "Medal", "medal_pro_claymorec4: " . $sqlquery );
+
+			$topplayer = ReturnMedalValue($sqlquery);
+			if ( isset($topplayer['PLAYERID']) )
+				InsertOrUpdateMedalValue(	"medal_pro_claymorec4", 
+											$content['medals']['medal_pro_claymorec4']['DisplayName'], 
+											$serverid, 
+											"medal_pro_claymorec4", 
+											$topplayer['AllKills'], 
+											"Kills", 
+											$topplayer['PLAYERID'],
+											4 );
+			else
+				PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_claymorec4 is empty!" );
+		}
+		// --- 
 	}
 
 	// --- Calc: medal_pro_pistol
@@ -519,32 +558,6 @@ function CreateAllMedals( $serverid )
 			PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_pistol is empty!" );
 	}
 	// --- 
-
-	// Special COD4 Medals!
-	if($content['gen_gameversion'] == COD4)
-	{
-		// --- Calc: medal_pro_claymorec4
-		if ( $content["medal_pro_claymorec4"] == "yes" ) 
-		{
-			// Credits for this medal go to: [-UFC-]James Ryan (matze1)
-			$sqlquery =	$content['medals']['medal_pro_claymorec4']['sql'] . " DESC LIMIT 1";
-			PrintHTMLDebugInfo( DEBUG_ULTRADEBUG, "Medal", "medal_pro_claymorec4: " . $sqlquery );
-
-			$topplayer = ReturnMedalValue($sqlquery);
-			if ( isset($topplayer['PLAYERID']) )
-				InsertOrUpdateMedalValue(	"medal_pro_claymorec4", 
-											$content['medals']['medal_pro_claymorec4']['DisplayName'], 
-											$serverid, 
-											"medal_pro_claymorec4", 
-											$topplayer['AllKills'], 
-											"Kills", 
-											$topplayer['PLAYERID'],
-											4 );
-			else
-				PrintHTMLDebugInfo( DEBUG_INFO, "Medal", "medal_pro_claymorec4 is empty!" );
-		}
-		// --- 
-	}
 	// ==========================            =================================
 
 /*  *** ANTI MEDAL CODE REMOVED BY REQUEST ***
